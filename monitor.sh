52bd01312375da11db78e8b8ee597169ebbe61ef
Ondřej <ondrej@pop-os.home>
2025-12-28 17:23:54 +0100

    started with my first monitor script

---
+++ monitor.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Simple system monitoring script
+# i want this thing to refresh every second and show CPU, RAM, and disk usage as output.
+
+# so let's start with creating an infinite interruptable loop:
+while true; do
+...
+done

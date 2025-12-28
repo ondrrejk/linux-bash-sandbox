d9f2e607dd35a6c170b884f15478098cc3cf563f
Ondřej <ondrej@pop-os.home>
2025-12-28 18:11:54 +0100

    basic linux cmds

--- linux-basics/script1.sh
+++ linux-basics/script1.sh
@@ -56,7 +56,24 @@ cat helloworld.txt
 # some basic cat options
 cat -d # numbers all output lines, starting at 1
 cat -n # numbers all non-blank lines, ignoring empty lines
-cat -s # supresses repeated blank lines - replaces multiple consecutive blank lines into single blank line
+cat -s # supresses repeated blank lines => replaces multiple consecutive blank lines into single blank line
 cat -E # shows $ at the end of each line, making line boundaries visible
-# copy contents of file
+# copy file or dir
+cp #[options] [source] [destination]
 cp helloworld.txt /home/johndoe/ # copies the helloworld.txt file into johndoe directory without overwriting the original file
+cp helloworld.txt helloworld1.txt # creates a new helloworld1.txt file and copies the helloworld.txt file contents into the new destination file
+# you can also use multiple sources with destination at the end
+cp helloworld.txt helloworld1.txt /home/johndoe/ # copies both files into destination dir and replaces existing files with the same filename
+cp -n # prevents overwriting of existing destination files
+cp -i # interactive mode => if some file is going to be overwritten, the cmdline asks you before overwriting it
+# use . to say pwd
+# go 1 step back in directory tree by using ../
+cp ../somefile1.txt ../somefile2.txt . # copies both files into pwd
+# recursive copying => directory that contains files cannot be copied into an new unexisting directory
+cp somedir1 somedir3 # error -> omits somedir1
+cp -r somedir1 somedir3 # successfully recursively copies somedir1 with it's contents into somedir3
+# let's break it down... somedir1 cannot be copied into a new unexisting dir because it first need to transfer the somedir1 into somedir3, and then transfer the files
+cp -vR somedir1 somedir3 # -v for verbose:
+# 'somedir1' -> 'somedir3/somedir1'
+# 'somedir1/somefile1.txt' -> 'somedir3/somedir1/somefile1.txt'
+# 'somedir1/somefile2.txt' -> 'somedir3/somedir1/somefile2.txt'
